2ce385ea718e0de059150480fbf16a98128fe3d2
User Name <landonisaiah@gmail.com>
2025-10-15 00:25:30 -0700

    render new form

--- controllers/foods.js
+++ controllers/foods.js
@@ -5,19 +5,13 @@ const User = require('../models/user.js');
 
 router.get('/', async (req, res) => {
     try {
-        console.log('PANTRY ROUTE DEBUG');
-        console.log('req.params.userId:', req.params.userId);
-        console.log('req.session.user:', req.session.user);
-
+        
         const user = await User.findById(req.params.userId);
-        console.log('User found:', user);
-
+        
         if (!user) {
-            console.log('User not found, redirecting to home.');
             return res.redirect('/');
         }
 
-        console.log('Rendering pantry with:', user.pantry);
         res.render('foods/index.ejs', {
             pantry: user.pantry,
         });
@@ -25,6 +19,10 @@ router.get('/', async (req, res) => {
         console.log('Error in patry route:', err);
         res.redirect('/');
     }
+});
+
+router.get('/new', (req, res) => {
+    res.render('foods/new.ejs');
 });
 
 module.exports = router;
