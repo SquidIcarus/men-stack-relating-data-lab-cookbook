7f5354789ac3aae9d7bd100b4f79c3897d86776d
User Name <landonisaiah@gmail.com>
2025-10-15 00:09:59 -0700

    add mergeParams in foods router to access userId

--- controllers/foods.js
+++ controllers/foods.js
@@ -1,8 +1,30 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 const User = require('../models/user.js');
 
-// router logic will go here - will be built later on in the lab
+router.get('/', async (req, res) => {
+    try {
+        console.log('PANTRY ROUTE DEBUG');
+        console.log('req.params.userId:', req.params.userId);
+        console.log('req.session.user:', req.session.user);
+
+        const user = await User.findById(req.params.userId);
+        console.log('User found:', user);
+
+        if (!user) {
+            console.log('User not found, redirecting to home.');
+            return res.redirect('/');
+        }
+
+        console.log('Rendering pantry with:', user.pantry);
+        res.render('foods/index.ejs', {
+            pantry: user.pantry,
+        });
+    } catch (err) {
+        console.log('Error in patry route:', err);
+        res.redirect('/');
+    }
+});
 
 module.exports = router;
